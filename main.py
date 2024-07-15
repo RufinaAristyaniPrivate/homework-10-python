e1604344824335bd4c9cb50b3307593d7feab61d
RufinaAristyaniPrivate <rufina.aristyani08@gmail.com>
2024-07-15 16:55:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import fibonacci
+
+def fibonacci(limit):
+    fibonacci_numbers = []
+    a, b = 0, 1
+    
+    for _ in range(limit):
+        fibonacci_numbers.append(a)
+        a, b = b, a + b
+        
+    return fibonacci_numbers
